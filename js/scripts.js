c31407d7fc47d0d669c1a8bfd99aa8a0674cfbbc
alban Alban Blanchard <alban.blanchard@gmail.com>
2017-10-11 23:54:16 +0200

    reglages shrink header

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+// shrink header on scroll
+$(document).on("scroll", function(){
+		if
+      ($(document).scrollTop() > 100){
+		  $("header").addClass("shrink-header");
+      $("#me").addClass("shrink-me");
+      $("#logo-ab").addClass("shrink-logo-ab");
+      $("#logo").addClass("shrink-logo");
+		}
+		else
+		{
+			$("header").removeClass("shrink-header");
+      $("#me").removeClass("shrink-me");
+      $("#logo-ab").removeClass("shrink-logo-ab");
+      $("#logo").removeClass("shrink-logo");
+		}
+	});
+
+// smooth scroll with nav
+  $(document).ready(function () {
+    // bind click event to all internal page anchors
+    $('a[href*="#"]').on('click', function (e) {
+        // prevent default action and bubbling
+        e.preventDefault();
+        e.stopPropagation();
+        // set target to anchor's "href" attribute
+        var target = $(this).attr('href');
+        // scroll to each target
+        $(target).velocity('scroll', {
+            duration: 1000,
+            offset: -75,
+            easing: 'easeOutCirc'
+            });
+        });
+      });
