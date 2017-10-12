2876d0b1c026e21c0f37fa783e9779278b678418
alban Alban Blanchard <alban.blanchard@gmail.com>
2017-10-12 12:30:59 +0200

    debug hamburger menu en cours

--- js/scripts.js
+++ js/scripts.js
@@ -16,8 +16,9 @@ $(document).on("scroll", function(){
 		}
 	});
 
-// smooth scroll with nav
+
   $(document).ready(function () {
+		// smooth scroll with nav
     // bind click event to all internal page anchors
     $('a[href*="#"]').on('click', function (e) {
         // prevent default action and bubbling
@@ -33,4 +34,33 @@ $(document).on("scroll", function(){
             easing: 'easeOutCirc'
             });
         });
+
+				// hamburger menu
+				// 0 = hide, 1 = visible
+			  var menuState = 0;
+			  //if($(".mini-menu-options").is(":hidden")) {
+			    /* Add a Click event listener to btn-select */
+			    $(".btn-select").on("click",function() {
+			      if(menuState === 0) {
+			        $(".mini-menu-options").slideDown("slow");
+			        menuState = 1;
+			      } else {
+			        $(".mini-menu-options").slideUp("slow");
+			        menuState = 0;
+			      }
+			    });
+			  //}
+			  $(".mini-menu-options li").on("click", function() {
+			    var numHijos = $(this).children().length;
+			    if(numHijos < 2) {
+			      // hide the menu
+			      $(".mini-menu-options").hide("fast");
+
+			      var texto = $(this).text();
+
+			      $(".menu-select .menu-actual").text(texto);
+			    }
+			    menuState = 0;
+			  });
+				
       });
